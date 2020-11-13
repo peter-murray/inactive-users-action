ddbd4c44d5963c25facf0a15e636946e8ca4c233
Peter Murray <681306+peter-murray@users.noreply.github.com>
2020-11-13 17:16:20 +0000

    Fixing typo in reference

--- dist/index.js
+++ dist/index.js
@@ -1452,14 +1452,6 @@ function generateUserActivityData(data) {
 
 /***/ }),
 
-/***/ 170:
-/***/ (function(module) {
-
-module.exports = eval("require")("./userActivityAttributes");
-
-
-/***/ }),
-
 /***/ 174:
 /***/ (function(module) {
 
@@ -4722,91 +4714,91 @@ exports.request = request;
 /***/ 245:
 /***/ (function(module, __unusedexports, __webpack_require__) {
 
-const UserActivityAttributes = __webpack_require__(170);
+const UserActivityAttributes = __webpack_require__(543);
 
 module.exports = class UserActivity {
 
-  constructor(login) {
-    this._login = login;
+    constructor(login) {
+        this._login = login;
 
-    const data = {};
-    Object.values(UserActivityAttributes).forEach(type => {
-      data[type] = {};
-    });
-    this._data = data;
-  }
-
-  get login() {
-    return this._login;
-  }
-
-  get email() {
-    return this._email || '';
-  }
-
-  set email(email) {
-    this._email = email;
-  }
-
-  get isActive() {
-    return (this.commits + this.pullRequestComments + this.issueComments + this.issues) > 0;
-  }
-
-  increment(attribute, repo, amount) {
-    if (Object.values(UserActivityAttributes).indexOf(attribute) > -1) {
-      if (!this._data[attribute][repo]) {
-        this._data[attribute][repo] = 0
-      }
-      this._data[attribute][repo] = this._data[attribute][repo] + amount;
-    } else {
-      throw new Error(`Unsupported attribute type '${attribute}'`);
-    }
-  }
-
-  get commits() {
-    return this._getTotal(UserActivityAttributes.COMMITS);
-  }
-
-  get pullRequestComments() {
-    return this._getTotal(UserActivityAttributes.PULL_REQUEST_COMMENTS);
-  }
-
-  get issues() {
-    return this._getTotal(UserActivityAttributes.ISSUES);
-  }
-
-  get issueComments() {
-    return this._getTotal(UserActivityAttributes.ISSUE_COMMENTS);
-  }
-
-  get jsonPayload() {
-    const self = this
-      , result = {
-      login: this.login,
-      email: this.email,
-      isActive: this.isActive
-    };
-
-    Object.values(UserActivityAttributes).forEach(type => {
-      result[type] = self._getTotal(type);
-    })
-
-    return result;
-  }
-
-  _getTotal(attribute) {
-    let total = 0;
-
-    if (this._data[attribute]) {
-      const values = this._data[attribute];
-
-      Object.keys(values).forEach(repo => {
-        total += values[repo];
-      });
+        const data = {};
+        Object.values(UserActivityAttributes).forEach(type => {
+            data[type] = {};
+        });
+        this._data = data;
     }
 
-    return total;
-  }
+    get login() {
+        return this._login;
+    }
+
+    get email() {
+        return this._email || '';
+    }
+
+    set email(email) {
+        this._email = email;
+    }
+
+    get isActive() {
+        return (this.commits + this.pullRequestComments + this.issueComments + this.issues) > 0;
+    }
+
+    increment(attribute, repo, amount) {
+        if (Object.values(UserActivityAttributes).indexOf(attribute) > -1) {
+            if (!this._data[attribute][repo]) {
+                this._data[attribute][repo] = 0
+            }
+            this._data[attribute][repo] = this._data[attribute][repo] + amount;
+        } else {
+            throw new Error(`Unsupported attribute type '${attribute}'`);
+        }
+    }
+
+    get commits() {
+        return this._getTotal(UserActivityAttributes.COMMITS);
+    }
+
+    get pullRequestComments() {
+        return this._getTotal(UserActivityAttributes.PULL_REQUEST_COMMENTS);
+    }
+
+    get issues() {
+        return this._getTotal(UserActivityAttributes.ISSUES);
+    }
+
+    get issueComments() {
+        return this._getTotal(UserActivityAttributes.ISSUE_COMMENTS);
+    }
+
+    get jsonPayload() {
+        const self = this,
+            result = {
+                login: this.login,
+                email: this.email,
+                isActive: this.isActive
+            };
+
+        Object.values(UserActivityAttributes).forEach(type => {
+            result[type] = self._getTotal(type);
+        })
+
+        return result;
+    }
+
+    _getTotal(attribute) {
+        let total = 0;
+
+        if (this._data[attribute]) {
+            const values = this._data[attribute];
+
+            Object.keys(values).forEach(repo => {
+                total += values[repo];
+            });
+        }
+
+        return total;
+    }
 }
 
 /***/ }),
