d50b0b587ad127ad598f7f2d1ea72175b203dae8
Shaun Alexander <shaunaa126@gmail.com>
2021-02-03 22:44:09 +0000

    remove trim

--- dist/index.js
+++ dist/index.js
@@ -42,7 +42,7 @@ async function run() {
   ;
 
   console.log(`Attempting to generate organization user activity data, this could take some time...`);
-  const orgs = organization.trim().split(",")
+  const orgs = organization.split(",")
   console.log(`orgs array:` + orgs);
   for (const org in orgs) {
     console.log(`Generating report for following org:` + org);
